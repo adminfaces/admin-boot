5cfcbac3087081452e1350863e76543377ab69e5
rmpestano <rmpestano@gmail.com>
2017-07-09 11:39:18 -0300

    add messages and fa.

--- src/main/java/com/github/adminfaces/boot/bean/InputMB.java
+++ src/main/java/com/github/adminfaces/boot/bean/InputMB.java
@@ -1,5 +1,7 @@
 package com.github.adminfaces.boot.bean;
 
+import org.omnifaces.util.Messages;
+
 import javax.faces.bean.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -11,14 +13,20 @@ import java.io.Serializable;
 @ViewScoped
 public class InputMB implements Serializable {
 
-    private String input;
+    private String name;
 
 
-    public String getInput() {
-        return input;
+    public String getName() {
+        return name;
     }
 
-    public void setInput(String input) {
-        this.input = input;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void doAction() {
+        Messages.create("Welcome to AdminBoot "+name+"!")
+                .detail("<b>AdminFaces</b> and <b>SpringBoot</b> integration via <b>JoinFaces.</b>")
+                .add();
     }
 }
